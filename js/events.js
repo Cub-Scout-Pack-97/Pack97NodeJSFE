a7eeff4a997b507065e3b67802988af5e707c860
John Kelley <john.r.kelley@lowes.com>
2018-09-21 17:11:41 -0400

    finish up event admin functionality

--- js/events.js
+++ js/events.js
@@ -10,3 +10,33 @@ $(window).on('click',(event) => {
 		$("#newAttendee").hide();
 	}
 });
+$("#sortdate").on('click',() => {
+	const URLarray = window.location.href.split('/');
+	const direction = URLarray[URLarray.length -1];
+	const by = URLarray[URLarray.length -2];
+	if(by === "event_date" && direction === "1"){
+		window.location.replace("/events/admin/list/event_date/-1");
+	}else{
+		window.location.replace("/events/admin/list/event_date/1");
+	}
+});
+$("#sortname").on('click',() => {
+	const URLarray = window.location.href.split('/');
+	const direction = URLarray[URLarray.length -1];
+	const by = URLarray[URLarray.length -2];
+	if(by === "event_name" && direction === "1"){
+		window.location.replace("/events/admin/list/event_name/-1");
+	}else{
+		window.location.replace("/events/admin/list/event_name/1");
+	}
+});
+$('#location').blur(function(){
+	let address = this.value.replace(/ /g,"+");
+	address = address.replace(/,/g,"%2C");
+	console.log(address);
+	$('#goog_maps').attr('href',"https://www.google.com/maps/embed/?api=1&query=" + address);
+});
+
+function showhide(id){
+	$(id).toggle();
+};
--- server.js
+++ server.js
@@ -3,84 +3,6 @@
 const Hapi = require('hapi');
 const Wreck = require('wreck');
 const server = new Hapi.Server();
-const events = {"events":[
-	{"event_name":"End of Year Pack Meeting/Pack Picnic Crossover",
-	"event_date":"Sat, Jun 2nd, 2019",
-	"reg_close":"04-21-2018",
-	"event_id":"227",
-	"event_desc":"2018 Super Trip to Gatlinburg TN. We will be sleeping with the sharks. Includes next day admission to Ripley's Aquarium. Must be at least 5 years old to attend!  For more information please see the",
-	"event_lead":"test",
-	"event_lead_email":"kittykolb@gmail.com",
-	"event_payment":"https://squareup.com/store/bsa-pack-97/item/summer-daycamp",
-	"participants":[
-		{"contact_name":"John Kelley",
-		"contact_phone":"7045551234",
-		"contact_email":"johnkelley4477@gmail.com",
-		"verified":true,
-		"attendees":[
-			{'type':'other','name':'Mason S.','detail':'Male'},
-			{'type':'scout','name':'Cameron S.','detail':'Webelos: Nifflers'},
-			{'type':'leader','name':'John K.','detail':'Male'},
-			{'type':'adult','name':'Katie B.','detail':'Female'},
-			{'type':'scout','name':'Cooper S.','detail':'Tiger: Den 3'}
-		]},
-		{"contact_name":"James Forbes",
-		"contact_phone":"7045551234",
-		"contact_email":"johnkelley4477@gmail.com",
-		"verified":false,
-		"attendees":[
-			{"type":"other","name":"Everett F..","detail":"Male"},
-			{"type":"adult","name":"James F.","detail":"Male"},
-			{"type":"adult","name":"Chris F.","detail":"Female"},
-			{"type":"scout","name":"Travis F.","detail":"Tiger: Den 3"}
-		]}
-	]},
-	{"event_name":"End of Year Pack Meeting/Pack Picnic Crossover",
-	"event_date":"Sat, Jun 2nd, 2018",
-	"reg_close":"04-21-2018",
-	"event_id":"226",
-	"event_desc":"2018 Super Trip to Gatlinburg TN. We will be sleeping with the sharks. Includes next day admission to Ripley's Aquarium. Must be at least 5 years old to attend!  For more information please see the",
-	"event_lead":"test",
-	"event_lead_email":"kittykolb@gmail.com",
-	"event_payment":"https://squareup.com/store/bsa-pack-97/item/summer-daycamp",
-	"participants":[
-		{"contact_name":"John Kelley",
-		"contact_phone":"7045551234",
-		"contact_email":"johnkelley4477@gmail.com",
-		"verified":true,
-		"attendees":[
-			{"type":"other","name":"Mason S.","detail":"Male"},
-			{"type":"scout","name":"Cameron S.","detail":"Webelos: Nifflers"},
-			{"type":"leader","name":"John K.","detail":"Male"},
-			{"type":"adult","name":"Katie B.","detail":"Female"},
-			{"type":"scout","name":"Cooper S.","detail":"Tiger: Den 3"}
-		]}
-	]}
-]};
-const leaders = {"contacts":[
-	{"scout_id":"8928398298392801",
-	"contact_name":"John Kelley",
-	"contact_title":"Tiger Den Leader",
-	"contact_desc":"Long hikes in the woods",
-	"contact_email":"johnkelley@cubscoutspack97.com"},
-	{"scout_id":"1241356465461223",
-	"contact_name":"James Barton",
-	"contact_title":"Arrow of Light Patrol Leader",
-	"contact_desc":"Long walks on the beach",
-	"contact_email":"jamesbarton@cubscoutspack97.com"}
-]};
-const committee = {"contacts":[
-	{"scout_id":"8928398298392801",
-	"contact_name":"John Kelley",
-	"contact_title":"Webmaster",
-	"contact_desc":"Long hikes in the woods",
-	"contact_email":"webmaster@cubscoutspack97.com"},
-	{"scout_id":"1241356465461223",
-	"contact_name":"Micheal Kolb",
-	"contact_title":"Committee Chair",
-	"contact_desc":"Long walks on the beach",
-	"contact_email":"committeechair@cubscoutspack97.com"}
-]};
 
 server.register([require('vision'),require('inert')], (err) => {
 
@@ -269,13 +191,31 @@ server.route({
 	method:'POST',
 	path:'/events/register/attendee',
 	handler: async (request,reply) => {
-		//console.log(request.payload);
 		const wreck = await Wreck.post('http://10.5.0.7:4477/api/pack97/event/add/attendees', { payload: request.payload }, (err, res, payload) => {
 		    console.log(`Error ${err}`)
 		});
 		reply.redirect('/events');
 		return true; 
 		
+	}
+});
+
+server.route({
+	method:'POST',
+	path:'/event/save',
+	handler: async (request,reply) => {
+		let payload = request.payload;
+		payload.enabled = checkToBoolean(request.payload.enabled);
+		payload.visible = checkToBoolean(request.payload.visible);
+		payload.lead_notify = checkToBoolean(request.payload.lead_notify);
+		payload.backpacking = checkToBoolean(request.payload.backpacking);
+		payload.tshirt = checkToBoolean(request.payload.tshirt);
+		
+		const wreck = await Wreck.post('http://10.5.0.7:4477/api/pack97/event/new', { payload: request.payload }, (err, res, payload) => {
+		    console.log(`Error ${err}`)
+		});
+		reply.redirect('/events/admin/list/event_date/1');
+		return true; 
 	}
 });
 
@@ -306,9 +246,68 @@ server.route({
 	}
 });
 
+server.route({
+	method:'GET',
+	path:'/events/admin/list/{field}/{direction}',
+	handler: async (request,reply) => {
+		const field = request.params.field;
+		const direction = request.params.direction;
+		const {res,payload} = await Wreck.get(`http://10.5.0.7:4477/api/pack97/event/list/${field}/${direction}`);
+		let data = {"events":JSON.parse(payload)};
+		return reply.view('events_admin_list',data);
+	}
+});
+
+server.route({
+	method:'GET',
+	path:'/events/admin/event',
+	handler:(request,reply) =>{
+		const pay = {"path":"/event/save"};
+		reply.view('eventbuild',pay);
+	}
+});
+
+server.route({
+	method:'GET',
+	path:'/events/admin/event/{id}',
+	handler: async(request,reply) =>{
+		const {res,payload} = await Wreck.get(`http://10.5.0.7:4477/api/pack97/event/${request.params.id}`);
+		const pay = JSON.parse(payload);
+		pay.path = "/event/update";
+		reply.view('eventbuild',pay);
+	}
+});
+
+server.route({
+	method:'POST',
+	path:'/event/update',
+	handler: async(request,reply) => {
+		let payload = request.payload;
+		payload.enabled = checkToBoolean(request.payload.enabled);
+		payload.visible = checkToBoolean(request.payload.visible);
+		payload.lead_notify = checkToBoolean(request.payload.lead_notify);
+		payload.backpacking = checkToBoolean(request.payload.backpacking);
+		payload.tshirt = checkToBoolean(request.payload.tshirt);
+		console.log("front end \n" + JSON.stringify(payload) + "\n\n\n");
+		const wreck = await Wreck.post('http://10.5.0.7:4477/api/pack97/event/update', { payload: payload }, (err, res, payload) => {
+		    console.log(`Error ${err}`)
+		});
+		reply.redirect('/events/admin/list/event_date/1');
+		return true;
+	}
+});
+
 server.start((err) => {
     if (err) {
         throw err;
     }
     console.log(`Server running at: ${server.info.uri}/`);
 });
+
+function checkToBoolean(checkbox){
+	let bool = false;
+	if (checkbox === 'on'){
+		bool = true;
+	}
+	return bool;
+}
